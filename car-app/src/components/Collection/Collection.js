0fd5c687a60f81003f71f0820af85b92ffc800ae
NikolaGrigorov0 <129086579+NikolaGrigorov0@users.noreply.github.com>
2023-03-30 22:01:33 +0300

    finaly almost did collection

--- car-app/src/components/Collection/Collection.js
+++ car-app/src/components/Collection/Collection.js
@@ -1,27 +1,20 @@
 import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../contexts/authContext';
+import { CarItem } from './CarItem';
 import './Collection.css'
 
 
-export const Collection = () => {
+export const Collection = ( { cars } ) => {
+
   const { isAuthenticated } = useContext(AuthContext);
+
     return(
             <div className="car-collection">
-        <div className="car-item">
-          <div className="car-image" > {/* style="background-image: url('car2.jpg')" */} </div>
-          <div className="car-model">Audi</div>
-          <div className="create-year">2010</div>
-          <div className="car-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed commodo enim eu quam fermentum, eu ultricies velit luctus. Duis vel ex mauris. </div>
-          {isAuthenticated && (<Link to='/details' className="details-btn"> Details </Link>)}
-        </div>
-        <div className="car-item">
-          <div className="car-image" /* style="background-image: url('car2.jpg')" */></div>
-          <div className="car-model">BMW</div>
-          <div className="create-year">2010</div>
-          <div className="car-description">Pellentesque et vestibulum purus. Duis aliquam justo odio, in malesuada neque commodo eget. Vivamus pharetra metus sed erat molestie, ac eleifend lacus commodo. </div>
-          {isAuthenticated && (<Link to='/details' className="details-btn"> Details </Link>)}
-        </div>
+        { cars.map(car => <CarItem key={car._id} {...car} />) } 
+       
+            { cars.length === 0  && <h1>Empty array</h1>}
         </div>
     );
+    
 }
--- car-app/src/services/authService.js
+++ car-app/src/services/authService.js
@@ -1,7 +1,7 @@
 import * as request from "./requester";
 
-const baseUrl = 'http://localhost:3030';
+const baseUrl = 'http://localhost:3030/users';
 
-export const register = async (data) => request.post(`${baseUrl}/users/register`, data);
+export const register = async (data) => request.post(`${baseUrl}/register`, data);
 
-export const login = async (data) => request.post(`${baseUrl}/users/login`, data);
+export const login = async (data) => request.post(`${baseUrl}/login`, data);
